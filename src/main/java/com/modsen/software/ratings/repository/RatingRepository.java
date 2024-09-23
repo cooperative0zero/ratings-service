2f187f0d786d8361b5d58ffacf4eded9b5b8e80b
aleksej <man747704@gmail.com>
2024-09-23 07:48:21 +0300

    feat(repository): add repository with liquibase changelogs

---
+++ src/main/java/com/modsen/software/ratings/repository/RatingRepository.java
@@ -0,0 +1,22 @@
+package com.modsen.software.ratings.repository;
+
+import com.modsen.software.ratings.entity.Rating;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import java.util.Date;
+
+@Repository
+public interface RatingRepository extends JpaRepository<Rating, Long> {
+    Page<Rating> findAllByPassengerId(Long passengerId, Pageable pageable);
+    Page<Rating> findAllByDriverId(Long driverId, Pageable pageable);
+
+    @Modifying
+    @Query("UPDATE Rating rt SET rt.rating = :rating, creationDate = :creationDate WHERE rt.id = :id")
+    int changeRating(@Param("id") Long id, @Param("rating") Byte rating, @Param("creationDate") Date date);
+}
